ba3cd9d46f4c2acc9588823610731f9c900b6785
Daniel Hill <danielhill10@bellsouth.net>
2017-12-13 16:30:50 -0500

    Initial code commit.

---
+++ Speq/Source/ViewModels/Core/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speq.Source.ViewModels.Core {
+    public abstract class ViewModel : INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler PropertyChanged = ( sender, args ) => { };
+
+        protected virtual void OnPropertyChanged ( [CallerMemberName] string property = null ) {
+            PropertyChanged( this, new PropertyChangedEventArgs( property ) );
+        }
+
+    }
+}
